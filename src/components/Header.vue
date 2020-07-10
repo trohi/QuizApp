ae06c2cfc5f0ca08e9a6f499807f8ae0cc839746
trohi <dzenan_95@hotmail.com>
2020-07-10 12:58:48 +0200

    improve in correct answers functionality

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+  <b-nav tabs>
+    <b-nav-item disabled><b> Quiz app </b></b-nav-item>
+    <b-nav-item disabled>Counter: {{ numbCorrect }}/{{ numbTotal }}</b-nav-item>
+  </b-nav>
+</div>
+</template>
+
+<script>
+
+export default { 
+  props: [
+    'numbCorrect',
+    'numbTotal'
+  ]
+}
+</script>
